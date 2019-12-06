c4f96b98e08e7b4322797489d23b73c804cbaa06
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-06 16:40:19 +0300

    out

--- js/script.js
+++ js/script.js
@@ -3,4 +3,6 @@ var number = Math.floor(10 * Math.random()) + 1;
 document.getElementById('check').onclick = function() {
     var usernum = document.getElementById('mynum').value; 
     usernum = parseInt(usernum);
+
+    var out = document.getElementById('out');
 }
