3c8c7472ecf2ee061db35034d8f386f869e739b2
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-06 18:16:02 +0300

    полная игра

--- js/script.js
+++ js/script.js
@@ -15,6 +15,11 @@ function fl() {
         out.innerHTML = 'Вы угадали';
     } else if (num > prNum) {
         out.innerHTML = 'Число больше чем загаданное';
+        if (num > 10) {
+            out.innerHTML = 'Число должно быть меньше 10';
+        }
+    } else if (num < 1) {
+        out.innerHTML = 'Число должно быть больше 0';
     } else {
         out.innerHTML = 'Вы ввели число меньше задуманного'
     }
