d4bf90f83583b0deda97aec01a9a9dc977791648
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-06 17:37:58 +0300

    out

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var prNum, tempOut;
+prNum = Math.floor((Math.random() * 10) + 1);
+
+function fl() {
+    var num, out;
+
+    num = document.getElementById('mynum').value;
+    out = document.getElementById('out');
+}
