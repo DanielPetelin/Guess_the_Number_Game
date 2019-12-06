1ff29104368b00d7479c102a2cd4c9456afdafb2
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-06 16:57:23 +0300

    start

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 var number = Math.floor(10 * Math.random()) + 1;
 console.log(number);
 
-document.getElementById('check').onclick = function() {
-    var usernum = document.getElementById('mynum').value; 
-    usernum = parseInt(usernum);
+var usernum = document.getElementById('mynum'); 
+usernum = parseInt(usernum);
+var out = document.getElementById('out');
 
-    var out = document.getElementById('out');
     if (usernum == number) {
         out.innerHTML = 'Вы угадали';
     }
-}
