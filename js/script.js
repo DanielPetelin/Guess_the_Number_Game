c38033fb702fd0d9d5bb31332ec3f33bb3f029ed
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-06 16:38:45 +0300

    usernum

--- js/script.js
+++ js/script.js
@@ -2,4 +2,5 @@ var number = Math.floor(10 * Math.random()) + 1;
 
 document.getElementById('check').onclick = function() {
     var usernum = document.getElementById('mynum').value; 
+    usernum = parseInt(usernum);
 }
